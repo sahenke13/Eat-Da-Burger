abc498b1d42969696c3cc504b87e41aab038ab67
sahenke13 <shenke13@gmail.com>
2018-10-29 12:22:13 -0500

    EAT-DA-BURGER V3.2

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 var mysql = require("mysql");
+var connetion;
 
-var connection = mysql.createConnection({
+
+
+if(process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
